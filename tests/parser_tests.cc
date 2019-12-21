483d0ea9ee92495145493d4e4879b4f67b08ef8b
Chris Shyi <chris.shyi@mail.utoronto.ca>
2019-12-21 14:18:06 +0800

    Add testing
    Added catch header file and updated CMakeLists.txt to include test
    building information. Wrote a simple test to see if everything is set up
    correctly.

---
+++ tests/parser_tests.cc
@@ -0,0 +1,23 @@
+#include "catch.hpp"
+#include <string>
+#include "../include/browser_engine/parser.h"
+#include "../include/browser_engine/node.h"
+
+using std::string;
+
+TEST_CASE("Simple HTML parsing test", "[parser]") {
+
+    string test_str {"<html> <head foo=\"bar\"> </head> </html>"};
+
+    Node root = parse(test_str);
+
+    REQUIRE(root.get_children_ref().size() == 1);
+    REQUIRE(root.get_node_type() == NodeType::ELEMENT);
+    Node child = root.get_children_ref().at(0);
+    REQUIRE(child.get_node_type() == NodeType::ELEMENT);
+    auto data = std::get<ElementData>(child.get_node_data());
+    auto prop_map = data.get_attr_map();
+    auto search = prop_map.find("foo");
+    REQUIRE(search->first == "foo");
+    REQUIRE(search->second == "bar");
+}
